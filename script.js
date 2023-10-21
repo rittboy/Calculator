fa019e028aad0373276b96a0a615ec273c70e90a
John Ritter <john.ritter.8788@gmail.com>
2023-10-21 17:19:19 -0400

    Adding functionality to calculator, still a couple things need to be added.

--- script.js
+++ script.js
@@ -1,99 +1,138 @@
-// Function to calculate the sum of two numbers from user input
-function calculateSum() {
-    // Get the value from the input field with ID 'num1' and convert it to a floating point number
-    const num1 = parseFloat(document.getElementById('num1').value);
-    
-    // Get the value from the input field with ID 'num2' and convert it to a floating point number
-    const num2 = parseFloat(document.getElementById('num2').value);
-    
-    // Get a reference to the HTML element with ID 'result' to display results or messages later
-    const resultDiv = document.getElementById('result');
+//Creating class called Calculator
+class Calculator {
+    /**
+     * defines constructor for Calculator class
+     */
+    constructor() {
+        //initializes all display elements
+        this.previousOperandElement = document.getElementById("previous-operand");
+        this.currentOperandElement = document.getElementById("current-operand");
+        this.display = document.getElementById("display");
+        this.clearButton = document.getElementById("clear");
+        this.signButton = document.getElementById("sign");
+        this.percentButton = document.getElementById("percent");
+        this.divideButton = document.getElementById("divide");
+        this.sevenButton = document.getElementById("seven");
+        this.eightButton = document.getElementById("eight");
+        this.nineButton = document.getElementById("nine");
+        this.multiplyButton = document.getElementById("multiply");
+        this.fourButton = document.getElementById("four");
+        this.fiveButton = document.getElementById("five");
+        this.sixButton = document.getElementById("six");
+        this.subtractButton = document.getElementById("subtract");
+        this.oneButton = document.getElementById("one");
+        this.twoButton = document.getElementById("two");
+        this.threeButton = document.getElementById("three");
+        this.addButton = document.getElementById("add");
+        this.zeroButton = document.getElementById("zero");
+        this.decimalButton = document.getElementById("decimal");
+        this.equalsButton = document.getElementById("equals");
 
-    // Check if either of the inputs (or both) are not valid numbers
-    if (isNaN(num1) || isNaN(num2)) {
-        // If either input is not a valid number, display an error message
-        resultDiv.textContent = "Please enter valid numbers.";
-        // Exit the function early since we don't want to continue if the inputs are invalid
-        return;
+        //set default values for operands and operation
+        this.currentOperand = "0";
+        this.previousOperand = "";
+        this.operation = undefined;
+
+        //updates calculator display
+        this.updateDisplay();
     }
 
-    // Calculate the sum of the two numbers
-    const sum = num1 + num2;
-    
-    // Display the result in the 'result' element
-    resultDiv.textContent = `The sum is: ${sum}`;
-}
-
-function calculateQuotient() {
-    // Get the value from the input field with ID 'num1' and convert it to a floating point number
-    const num1 = parseFloat(document.getElementById('num1').value);
-    
-    // Get the value from the input field with ID 'num2' and convert it to a floating point number
-    const num2 = parseFloat(document.getElementById('num2').value);
-    
-    // Get a reference to the HTML element with ID 'result' to display results or messages later
-    const resultDiv = document.getElementById('result');
-
-    // Check if either of the inputs (or both) are not valid numbers
-    if (isNaN(num1) || isNaN(num2)) {
-        // If either input is not a valid number, display an error message
-        resultDiv.textContent = "Please enter valid numbers.";
-        // Exit the function early since we don't want to continue if the inputs are invalid
-        return;
+    //Function that clears calculator
+    clear() {
+        this.currentOperand = "0";
+        this.previousOperand = "";
+        this.operation = undefined;
+    }
+    //method to delete last digit from current operand
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+    //method to append new number to current operand
+    appendNumber(number) {
+        //check for duplicate decimals
+        if (number === "." && this.currentOperand.includes(".")) return;
+        //replace 0 with new number if current number is 0
+        if (this.currentOperand === "0" && number !== ".") {
+            this.currentOperand = number;
+        } else {
+            //append new number to current number
+            this.currentOperand += number;
+        }
     }
 
-    // Calculate the sum of the two numbers
-    const quot = num1 / num2;
-    
-    // Display the result in the 'result' element
-    resultDiv.textContent = `The quotient is: ${quot}`;
-}
+    chooseOpeartion(operation) {
+        if (this.currentOperand === "") return;
+        if (this.previousOperand !== "") {
+            this.compute();
+        }
 
-function calculateProduct() {
-    // Get the value from the input field with ID 'num1' and convert it to a floating point number
-    const num1 = parseFloat(document.getElementById('num1').value);
-    
-    // Get the value from the input field with ID 'num2' and convert it to a floating point number
-    const num2 = parseFloat(document.getElementById('num2').value);
-    
-    // Get a reference to the HTML element with ID 'result' to display results or messages later
-    const resultDiv = document.getElementById('result');
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";
 
-    // Check if either of the inputs (or both) are not valid numbers
-    if (isNaN(num1) || isNaN(num2)) {
-        // If either input is not a valid number, display an error message
-        resultDiv.textContent = "Please enter valid numbers.";
-        // Exit the function early since we don't want to continue if the inputs are invalid
-        return;
+        this.updateDisplay();
     }
 
-    // Calculate the sum of the two numbers
-    const product = num1 *num2;
-    
-    // Display the result in the 'result' element
-    resultDiv.textContent = `The product is: ${product}`;
-}
-function calculateDiff() {
-    // Get the value from the input field with ID 'num1' and convert it to a floating point number
-    const num1 = parseFloat(document.getElementById('num1').value);
-    
-    // Get the value from the input field with ID 'num2' and convert it to a floating point number
-    const num2 = parseFloat(document.getElementById('num2').value);
-    
-    // Get a reference to the HTML element with ID 'result' to display results or messages later
-    const resultDiv = document.getElementById('result');
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
 
-    // Check if either of the inputs (or both) are not valid numbers
-    if (isNaN(num1) || isNaN(num2)) {
-        // If either input is not a valid number, display an error message
-        resultDiv.textContent = "Please enter valid numbers.";
-        // Exit the function early since we don't want to continue if the inputs are invalid
-        return;
+        if (isNaN(prev) || isNaN(current)) return;
+
+        switch (this.operation) {
+            case "+":
+                computation = prev + current;
+                break;
+            case "-":
+                computation = prev - current;
+                break;
+            case "*":
+                computation = prev * current;
+                break;
+            case "/":
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation.toString();
+        this.operation = undefined;
+        this.previousOperand = "";
     }
 
-    // Calculate the sum of the two numbers
-    const diff = num1 -num2;
-    
-    // Display the result in the 'result' element
-    resultDiv.textContent = `The difference is: ${diff}`;
+    getDisplayNumber(number){
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split(".")[0]);
+        const decimalDigits = stringNumber.split(".")[1];
+        let integerDisplay;
+
+        if(isNaN(integerDigits)){
+            integerDisplay = "";
+        }else{
+            integerDisplay = integerDigits.toLocaleString("en", {
+                maximumFractionDigits: 0,
+            });
+        }
+        if(decimalDigits != null){
+            return '${integerDisplay}.${decimalDigits}';
+        }else{
+            return integerDisplay;
+        }
+    }
+
+
+    updateDisplay(){
+        if(this.operation !== null){
+            this.display.value = `'${this.getDisplayNumber(this.previousOperand)}${this.operation} ${this.currentOperand ? this.currentOperand : ''}`;
+        }else{
+            this.display.value = this.getDisplayNumber(this.currentOperand);
+        }
+    }
 }
+
+const calculator = new Calculator();
+
+calculator.clearButton.addEventListener("click", () =>{
+    calculator.clear();
+})
